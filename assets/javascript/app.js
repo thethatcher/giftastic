3de6e618c5f36f0a52d9aa4514a985d6423c92ad
thethatcher <thethatcher@gmail.com>
2017-07-22 08:53:51 -0600

    changed theme to emotions. soccer players returned inaccurate results

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,28 @@
-var players = ["Kyle Beckerman","Nick Rimando","Lionel Messi"];
+var emotions = ["Happy","Sad","Angry","Envious"];
 var key = "fcfcb5d597d24a258d68c0871be63e9b";
 var queryURL;
 var $gifHolder = $("#gif-holder");
-drawBtn(players);
+drawBtn(emotions);
 
 function drawBtn(array){
 	$("#button-holder").empty();
 	for (var i = 0; i < array.length; i++) {
-		$("#button-holder").append('<button class="playerBtn">' + array[i] + '</button>');
+		$("#button-holder").append('<button class="emoteBtn">' + array[i] + '</button>');
 	}
 	console.log('drawBtn called');
 }
 
-$(document).on("click",".playerBtn", playerBtnClick);
+$(document).on("click",".emoteBtn", emoteBtnClicked);
 
 $("#entry").submit(function(event){
 	var $tempEntry = $(this).children().eq(2);
-	players.push($tempEntry.val());
-	drawBtn(players);
+	emotions.push($tempEntry.val());
+	drawBtn(emotions);
 	$tempEntry.val("");
 	event.preventDefault();
 });
 
-function playerBtnClick(){
+function emoteBtnClicked(){
 	console.log("success! " + $(this).text() + " button clicked");
 	queryURL = "https://api.giphy.com/v1/gifs/search?q=" + $(this).text() + "&api_key=" + key + "&limit=10&rating=g&rating=pg";
 	$.ajax({
