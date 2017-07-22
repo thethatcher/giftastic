facb263bc1920795482a16e2f58a20b87086086e
thethatcher <thethatcher@gmail.com>
2017-07-22 01:10:33 -0600

    loaded gifs onto the page from the API

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,7 @@
 var players = ["Kyle Beckerman","Nick Rimando","Lionel Messi"];
 var key = "fcfcb5d597d24a258d68c0871be63e9b";
 var queryURL;
+var $gifHolder = $("#gif-holder");
 drawBtn(players);
 
 function drawBtn(array){
@@ -23,12 +24,20 @@ $("#entry").submit(function(event){
 
 function playerBtnClick(){
 	console.log("success! " + $(this).text() + " button clicked");
-	queryURL = "https://api.giphy.com/v1/gifs/search?q=" + $(this).text() + "&api_key=" + key + "&limit=10";
+	queryURL = "https://api.giphy.com/v1/gifs/search?q=" + $(this).text() + "&api_key=" + key + "&limit=10&rating=g&rating=pg";
 	$.ajax({
 		url: queryURL,
 		method: "GET"
 	}).done(function(response){
 			console.log(response);
-		});
+			console.log(queryURL);
+			loadGIFs(response);
+	});
 }
 
+function loadGIFs(obj){
+	$gifHolder.empty();
+	for (var i = 0; i < obj.data.length; i++) {
+		$gifHolder.append('<img src="' + obj.data[i].images.fixed_height.url + '">');
+	}
+}
